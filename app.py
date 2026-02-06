88d5129756c1d87be5adb56adce2f19717c5103c
MURAT DANIS <aslan4u@me.com>
2026-02-06 15:28:00 +0100

    moved home.html template added handle für adding Author in db with response message moved home.html to the right folder

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask
+import flask
+
 from flask_sqlalchemy import SQLAlchemy
-from data_models import db, Author, Book
+import data_models
 import os
 
 #Initialising Flask Server
-app = Flask(__name__)
-app.run(debug=True, host='0.0.0.0', port=8080)
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return flask.render_template('home.html')
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if flask.request.method == 'POST':
+        new_author_object = data_models.Author(name=flask.request.form['name'],
+                            birth_date=flask.request.form['birthdate'],
+                            date_of_death=flask.request.form['date_of_death'])
+
+        data_models.db.session.add(new_author_object)
+        data_models.db.session.commit()
+        return f"Author {flask.request.form['name']} successfully added!"
+    else:
+        return flask.render_template('add_author.html')
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 
-db.init_app(app)
-
+data_models.db.init_app(app)
+app.run(debug=True, host='0.0.0.0', port=8080)
