defc463926701fe4993ba1d37ea9e82d995420cb
MURAT DANIS <aslan4u@me.com>
2026-02-06 15:41:00 +0100

    added handle für adding Book in db with response message added form in add_book.html

--- app.py
+++ app.py
@@ -27,6 +27,19 @@ def add_author():
         return flask.render_template('add_author.html')
 
 
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if flask.request.method == 'POST':
+        new_book_object = data_models.Book(isbn=flask.request.form['isbn'],
+                                           title=flask.request.form['title'],
+                                           publication_year=flask.request.form['publication_year'])
+        data_models.db.session.add(new_book_object)
+        data_models.db.session.commit()
+        return f"Book {flask.request.form['title']} successfully added!"
+    else:
+        return flask.render_template('add_book.html')
+
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
